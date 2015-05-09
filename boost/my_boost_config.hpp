41ecfe1db953d0be841c94e2daa10344e3ff701b
Frang <github@frang.com>
2015-05-09 09:52:48 -0700

    Boost update, and try to clean some extra warnings

--- boost/my_boost_config.hpp
+++ boost/my_boost_config.hpp
@@ -2,3 +2,6 @@
 
 // The following fixes the auto-link behavior on windows, since we build with layout=tagged
 #define BOOST_AUTO_LINK_TAGGED
+#ifdef BOOST_MSVC
+#  pragma warning (disable : 4619)
+#endif // BOOST_MSVC
